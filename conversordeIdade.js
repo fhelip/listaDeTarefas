ea1e7c1edd2554f6c82d6ddfb0fa2493d718167f
Felipe Barbosa Gonçalves <felipe.goncalves@KINGSTAR.BR>
2022-06-22 11:47:58 -0300

    exercicios basicados de javascript

---
+++ conversordeIdade.js
@@ -0,0 +1,1 @@
+//Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias.
---
+++ verificadorDeNumeros.js
@@ -0,0 +1,1 @@
+//Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo.
